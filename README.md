e3078caeef05be9dad349178be5f96c5f91ae2f1
Stephen Buchanan <stephen.buchanan@random.nu>
2017-11-01 13:12:03 +0100

    starting with bootstrap-styled forms

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # OSC Simulator
-How many times have I just needed to test my [OSC](http://opensoundcontrol.org/) setup is working properly? So many times.
+How many times have I just needed to test my [OSC](http://opensoundcontrol.org/) setup is working properly? So.  Many. Times.
 
 Here is an easy utility to test receiving and sending of OSC messages from a browser interface.
--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Jumbotron>
+          <h1>OSC Simulator</h1>
+        </Jumbotron>
+        <form>
+          <input type="text" class="form-control" placeholder="Text input" />
+        </form>
       </div>
     );
   }
