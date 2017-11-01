b5d8d3ef368c82dd2f6ac5fa47e0f59e38585cbe
Stephen Buchanan <stephen.buchanan@random.nu>
2017-11-01 15:38:45 +0100

    can now set up multiple instances, including "standalone" server instances (no websocket communication) and these communicate with each other

--- server/server.js
+++ server/server.js
@@ -5,23 +5,40 @@ const logger = require('winston-color');
 logger.transports.console.level = 'verbose';
 
 // ----------------------------------------------------------------------------
+// Command line arguments, standalone mode
+// ----------------------------------------------------------------------------
+let STANDALONE = false;
+const argv = require('yargs').argv
+if (argv.standalone) {
+  logger.info('standalone mode: will only use CLI, no websocket');
+  STANDALONE = true;
+}
+
+// ----------------------------------------------------------------------------
 // OSC stuff
 // ----------------------------------------------------------------------------
 const osc = require('node-osc');
-const PORT = 12345;
+let destination = {
+  ip: argv.destinationIp || "127.0.0.1",
+  port: argv.destinationPort || 12345
+}
+let listen = {
+  ip: argv.listenIp || destination.ip,
+  port: argv.listenPort || destination.port
+}
+logger.info('will send to', destination);
+logger.info('will listen on', listen);
 
 let socketClient = null;
 
-const oscServer = new osc.Server(PORT, '0.0.0.0');
+const oscServer = new osc.Server(listen.port, listen.ip);
 
 oscServer.on("message", function (msg, rinfo) {
   logger.info("received OSC message:", msg);
-  if (socketClient) {
+  if (socketClient && !STANDALONE) {
     socketClient.emit('message', msg);
   }
 });
-
-logger.info('OSC server listening on port', PORT);
 
 function sendOsc(address, data, ip = '127.0.0.1', port = 12345) {
   var client = new osc.Client(ip, port);
@@ -34,27 +51,29 @@ function sendOsc(address, data, ip = '127.0.0.1', port = 12345) {
 // ----------------------------------------------------------------------------
 // WebSocket stuff
 // ----------------------------------------------------------------------------
-const WS_PORT = 5000;
-const server = require('http').createServer();
+if (!STANDALONE) {
+  const WS_PORT = 5000;
+  const server = require('http').createServer();
 
-const io = require('socket.io')(server, {
-  pingInterval: 10000,
-  pingTimeout: 5000,
-  cookie: false
-});
-
-server.listen(WS_PORT);
-logger.info('WebSocket server listening on port', WS_PORT);
-
-io.on('connection', (socket) => {
-  logger.info('connected to client websocket:', socket.id);
-  socketClient = socket;
-
-  socket.on('message', (data) => {
-    logger.verbose('Websocket -> OSC', data);
-    sendOsc(data.address, data.data, data.ip, data.port)
+  const io = require('socket.io')(server, {
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
   });
-});
+
+  server.listen(WS_PORT);
+  logger.info('WebSocket server listening on port', WS_PORT);
+
+  io.on('connection', (socket) => {
+    logger.info('connected to client websocket:', socket.id);
+    socketClient = socket;
+
+    socket.on('message', (data) => {
+      logger.verbose('Websocket -> OSC', data);
+      sendOsc(data.address, data.data, data.ip, data.port)
+    });
+  });
+}
 
 
 // ----------------------------------------------------------------------------
